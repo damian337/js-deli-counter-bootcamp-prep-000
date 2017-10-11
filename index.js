31a8eab37da6ea32fb92cb37106d094c9c1b3f41
Damian Guillory <guillorywdamian@gmail.com>
2017-10-11 03:45:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var katzDeli = [];
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+    katzDeliLine.push(name);
+    return ("Welcome, " + `${name}` + ". You are number " + katzDeliLine.length + " in line.");
+}
+
+function nowServing() {
+    if (katzDeliLine.length === 0) {
+        return ("There is nobody waiting to be served!");    
+    }
+    else {
+        return katzDeliLine.slice(1);
+    }
+}
